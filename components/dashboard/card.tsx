1fa1c2840e0da7363f447f6560af21b47b4addeb
Enzo Machado <enzo653fm@gmail.com>
2024-07-23 07:50:29 -0300

    feat: create dashboard page design

--- components/dashboard/card.tsx
+++ components/dashboard/card.tsx
@@ -12,7 +12,7 @@ export const Card = ({ title, number, change }: CardProps) => {
 		<div className="bg-slate-100 dark:bg-slate-900 p-5 rounded-lg flex gap-5 cursor-pointer w-full hover:bg-slate-200 dark:hover-bg-slate-800 duration-200">
 			<MdSupervisedUserCircle size={24} />
 			<div className="flex flex-col gap-5">
-				<span className="">{title}</span>
+				<span className="title">{title}</span>
 				<span className="text-2xl font-medium">{number}</span>
 				<span className="text-sm font-light">
 					<span className={change > 0 ? "text-green-400" : "text-red-400"}>
